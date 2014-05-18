1a29d7cc9aaaef348dccaea251975e2dcc7c0284
okapusta <kapuh@ubuntu.com>
2014-05-18 12:59:17 +0200

    thread

--- doc/src/thread.rb
+++ doc/src/thread.rb
@@ -6,20 +6,13 @@ t = Thread.new(Injector.new(OpenStruct.new({}))) do |injector|
     sleep 0.01; reading = 0;
 
     while injector.gpio.read(injector.options.capacitor.pin) == LOW
-      reading += 1
+     reading += 1
     end
 
-    if reading < injector.options.activation_threshold
+    if reading > injector.options.activation_threshold
       injector.caching_service.set('start_time', Time.now)
     end
   end
 end
 t.abort_on_exception = true
-map Skirace::Application.assets_prefix do
-  run Skirace::Application.sprockets
-end
 
-map '/' do
-  Skirace::Application.set :environment, ENV['RAKE_ENV']
-  run Skirace::Application
-end
