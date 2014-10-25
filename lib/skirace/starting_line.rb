add71b06f02325e9478fc65ed14a69cc06875abf
okapusta <kapuh@ubuntu.com>
2014-10-25 10:10:36 +0200

    chnage register to include, add logger

--- lib/skirace/starting_line.rb
+++ lib/skirace/starting_line.rb
@@ -1,7 +1,9 @@
 module Skirace
   module StartingLine
-    def self.registered(app)
+    def self.included(mod)
       Thread.new(Injector.new) do |i|
+        file = File.open(File.join(app.root,'log','reading.log'), "a")
+
         while true
           reading = 0
           i.capacitor.discharge(i.options.capacitor.pin)
@@ -9,7 +11,7 @@ module Skirace
           while i.gpio.read(i.options.capacitor.pin) == LOW
             reading +=1
           end
-
+          file.write("#{reading}\n")
           if reading > i.options.activation_threshold
             i.contestant_repository.set_start_time
           end
