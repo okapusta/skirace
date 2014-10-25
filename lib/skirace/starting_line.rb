9b973a4641d4ac8cf0a03db89d2d12f99914f2ca
okapusta <kapuh@ubuntu.com>
2014-10-25 10:44:00 +0200

    log exceptions

--- lib/skirace/starting_line.rb
+++ lib/skirace/starting_line.rb
@@ -5,18 +5,23 @@ module Skirace
         file = File.open(File.join(app.root,'log','reading.log'), "a")
 
         while true
-          reading = 0
-          i.capacitor.discharge(i.options.capacitor.pin)
+          begin
+            reading = 0
+            i.capacitor.discharge(i.options.capacitor.pin)
 
-          while i.gpio.read(i.options.capacitor.pin) == LOW
-            reading +=1
-          end
-          file.write("#{reading}\n")
-          if reading > i.options.activation_threshold
-            i.contestant_repository.set_start_time
-          end
+            while i.gpio.read(i.options.capacitor.pin) == LOW
+              reading +=1
+            end
+            file.write("#{reading}\n")
+            if reading > i.options.activation_threshold
+              i.contestant_repository.set_start_time
+            end
 
-          sleep i.options.measurement_accuracy
+            sleep i.options.measurement_accuracy
+          rescue => e
+            file.write("exception occured: #{e.message} \n")
+            file.write(e.backtrace.join("\n"))
+          end
         end
       end
     end
