c453e5ab04d1903b80fc88c22a0d4b3d1b205e8c
okapusta <kapuh@ubuntu.com>
2014-10-26 09:13:28 +0100

    puts

--- lib/skirace/starting_line.rb
+++ lib/skirace/starting_line.rb
@@ -2,26 +2,19 @@ module Skirace
   class StartingLine
     def initialize(path)
       Thread.new(Injector.new) do |i|
-        file = File.open(File.join(path, 'log','reading.log'), "a")
-
         while true
-          begin
-            reading = 0
-            i.capacitor.discharge(i.options.capacitor.pin)
+          reading = 0
+          i.capacitor.discharge(i.options.capacitor.pin)
 
-            while i.gpio.read(i.options.capacitor.pin) == LOW
-              reading +=1
-            end
-            file.write("#{reading}\n")
-            if reading > i.options.activation_threshold
-              i.contestant_repository.set_start_time
-            end
-
-            sleep i.options.measurement_accuracy
-          rescue => e
-            file.write("exception occured: #{e.message} \n")
-            file.write(e.backtrace.join("\n"))
+          while i.gpio.read(i.options.capacitor.pin) == LOW
+            reading +=1
           end
+          p reading
+          if reading > i.options.activation_threshold
+            i.contestant_repository.set_start_time
+          end
+
+          sleep i.options.measurement_accuracy
         end
       end
     end
