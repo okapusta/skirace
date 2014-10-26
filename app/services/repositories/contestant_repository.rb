a7d991d85e37dd41bf80dbfc3be6c246f55201db
okapusta <kapuh@ubuntu.com>
2014-10-26 13:25:27 +0100

    fix migration

--- app/services/repositories/contestant_repository.rb
+++ app/services/repositories/contestant_repository.rb
@@ -1,5 +1,5 @@
 class Repositories::ContestantRepository
-  takes :db_contestant, :time_service, :time, :caching_service
+  takes :db_contestant, :db_contest, :time_service, :time, :caching_service
 
   def build(params)
     contestant = db_contestant.new(params[:contestant])
@@ -20,18 +20,20 @@ class Repositories::ContestantRepository
   end
 
   def first(options = {start_time_at: nil})
-    current_contest = caching_service.get('current_contest')
-    options.merge!({contest_id: current_contest}) if current_contest
+    options.merge!({contest_id: current_contest})
     
     db_contestant.where(options).order(:id).first
   end
 
   def set_start_time
-    first.update(start_time_at: Time.now)
+    contestant = first || db_contestant.create(contest_id: current_contest)
+
+    contestant.update(start_time_at: Time.now)
   end
 
   def set_end_time(end_time_at)
     contestant  = first("start_time_at is not null and end_time is null")
+
     end_time = time_service.format(
       time.parse(end_time_at) - contestant.start_time_at
       )
@@ -39,5 +41,11 @@ class Repositories::ContestantRepository
     contestant.update(end_time_at: end_time_at, end_time: end_time)
     
     contestant
+  end
+
+  private
+
+  def current_contest
+    caching_service.get('current_contest') || db_contest.last
   end
 end 
--- db/migrations/001_create_contestants.rb
+++ db/migrations/001_create_contestants.rb
@@ -3,8 +3,8 @@ Sequel.migration do
     create_table(:contestants) do
       primary_key :id
       Integer :contest_id
-      String :first_name, null: false
-      String :last_name, null: false
+      String :first_name
+      String :last_name
       DateTime :start_time_at
       DateTime :end_time_at
       String :end_time
