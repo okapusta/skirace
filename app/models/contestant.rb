7286cd86b0af3f3d5db25082132c55c9097d0cca
okapusta <kapuh@ubuntu.com>
2014-11-08 11:59:12 +0100

    remove contestant validation

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,11 +1,3 @@
 class Contestant < Sequel::Model(Sequel.sqlite('db/database.sqlite')[:contestants])
 
-  plugin :validation_helpers
-
-  def validate
-    super
-    validates_presence :first_name
-    validates_presence :last_name
-  end
-
 end
